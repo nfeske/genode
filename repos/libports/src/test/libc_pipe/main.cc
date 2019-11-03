59f2fd14e4a6ecfce487def1669d8d54588e09e0
Norman Feske <norman.feske@genode-labs.com>
2019-11-03 11:49:56 +0100

    DEBUG test/libc_pipe: instrumentation

--- repos/libports/src/test/libc_pipe/main.cc
+++ repos/libports/src/test/libc_pipe/main.cc
@@ -18,6 +18,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <base/log.h>
+using namespace Genode;
 
 enum { BUF_SIZE = 16*1024 };
 static char buf[BUF_SIZE];
@@ -34,10 +36,12 @@ void *read_pipe(void *arg)
 
 	while (num_bytes_read < BUF_SIZE) {
 
+	log("pipe read");
 		ssize_t res = read(pipefd[0],
 		                   &read_buf[num_bytes_read],
 		                   BUF_SIZE - num_bytes_read);
 
+	log("pipe read returned");
 		if (res < 0) {
 			fprintf(stderr, "Error reading from pipe\n");
 			exit(1);
@@ -54,6 +58,8 @@ void *read_pipe(void *arg)
 
 	reader_finished = true;
 
+
+	log("pipe read thread returns");
 	return 0;
 }
 
@@ -64,24 +70,31 @@ int main(int argc, char *argv[])
 	buf[0] = 1;
 	buf[BUF_SIZE - 1] = 255;
 
+	log("pipe");
 	int res = pipe(pipefd);
+	log("pipe2");
 	if (res != 0) {
 		fprintf(stderr, "Error creating pipe\n");
 		exit(1);
 	}
+	log("pipe3");
 
 	pthread_t tid;
 	pthread_create(&tid, 0, read_pipe, 0);
+	log("pipe4");
 
 	ssize_t bytes_written = write(pipefd[1], buf, BUF_SIZE);
+	log("pipe5");
 
 	if (bytes_written != BUF_SIZE) {
 		fprintf(stderr, "Error writing to pipe (bytes_written=%zd, BUF_SIZE=%zd)\n",
 		        bytes_written, (size_t)BUF_SIZE);
 		exit(1);
 	}
+	log("pipe6 wait for pthread_join");
 
 	pthread_join(tid, NULL);
+	log("pipe7");
 
 	printf("--- test finished ---\n");
 
