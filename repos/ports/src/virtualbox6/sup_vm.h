f96af4604c897f2448c223c72a3214c411d08fd8
Christian Helmuth <christian.helmuth@genode-labs.com>
2020-11-04 15:54:26 +0100

    vbox6: implement Vm as GVM with memobjs as expected

--- repos/ports/src/virtualbox6/sup_vm.h
+++ repos/ports/src/virtualbox6/sup_vm.h
@@ -17,31 +17,53 @@
 
 /* VirtualBox includes */
 #include <PGMInternal.h>
-#include <VBox/vmm/vm.h>
+#include <VBox/vmm/gvm.h>
 
 /* local includes */
 #include <sup.h>
 
 namespace Sup { struct Vm; }
 
-struct Sup::Vm : VM
+struct Sup::Vm : GVM
 {
+	static size_t _size(Cpu_count cpu_count)
+	{
+		/* expected size of GVM structure (taken from GVMMR0.cpp) */
+		return RT_ALIGN_32(RT_UOFFSETOF_DYN(GVM, aCpus[cpu_count.value]), PAGE_SIZE);
+	}
+
 	void init(PSUPDRVSESSION psession, Cpu_count cpu_count)
 	{
-		enmVMState       = VMSTATE_CREATING;
-		pVMR0ForCall     = (PVMR0)this;
-		pSession         = psession;
-		cbSelf           = sizeof(VM);
-		cCpus            = cpu_count.value;
-		uCpuExecutionCap = 100;  /* expected by 'vmR3CreateU()' */
+		/* alloc and emulate R0MEMOBJ */
+		size_t    const num_pages = _size(cpu_count) / PAGE_SIZE;
+		SUPPAGE * const pages     = (SUPPAGE *)RTMemAllocZ(sizeof(SUPPAGE)*num_pages);
+
+		for (size_t i = 0; i < num_pages; ++i)
+			pages[i] = { .Phys = (RTHCPHYS)this + i*PAGE_SIZE, .uReserved = 0 };
+
+		/*
+		 * Some members of VM also exist in GVM (e.g., pSession) therefore we
+		 * explicitly qualify which one is used.
+		 */
+
+		VM::enmVMState       = VMSTATE_CREATING;
+		VM::paVMPagesR3      = (R3PTRTYPE(PSUPPAGE))pages;
+		VM::pVMR0ForCall     = (PVMR0)this;
+		VM::pSession         = psession;
+		VM::cbSelf           = sizeof(VM);
+		VM::cbVCpu           = sizeof(VMCPU);
+		VM::cCpus            = cpu_count.value;
+		VM::uCpuExecutionCap = 100;  /* expected by 'vmR3CreateU()' */
 
 		for (uint32_t i = 0; i < cpu_count.value; ++i) {
-			apCpusR3[i] = (VMCPU *)RTMemAllocZ(sizeof(VMCPU));
-			log(this, ": apCpusR3[", i, "]=", apCpusR3[i]);
+			VMCPU &cpu = GVM::aCpus[i];
 
-			apCpusR3[i]->pVMR3           = this;
-			apCpusR3[i]->idHostCpu       = NIL_RTCPUID;
-			apCpusR3[i]->hNativeThreadR0 = NIL_RTNATIVETHREAD;
+			cpu.pVMR3           = this;
+			cpu.idHostCpu       = NIL_RTCPUID;
+			cpu.hNativeThreadR0 = NIL_RTNATIVETHREAD;
+
+			VM::apCpusR3[i] = &cpu;
+			log(this, ": apCpusR3[", i, "]=", apCpusR3[i]);
 		}
 
 //		pVM->apCpusR3[0]->hNativeThreadR0 = RTThreadNativeSelf();
@@ -57,7 +79,7 @@ struct Sup::Vm : VM
 		 * the R3 VM struct, which must be page-aligned and contains an array of
 		 * VMCPU pointers in apCpusR3.
 		 */
-		Vm *vm_ptr = (Vm *)RTMemPageAllocZ(sizeof(Vm));
+		Vm *vm_ptr = (Vm *)RTMemPageAllocZ(_size(cpu_count));
 
 		vm_ptr->init(psession, cpu_count);
 
