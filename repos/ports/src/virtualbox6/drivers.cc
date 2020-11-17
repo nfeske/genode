d66b55bbc403763ae836ebca3e81320b8b963ea8
Norman Feske <norman.feske@genode-labs.com>
2020-11-17 23:01:45 +0100

    vbox6: add MainKeyboard and MainMouse driver

--- repos/ports/src/virtualbox6/drivers.cc
+++ repos/ports/src/virtualbox6/drivers.cc
@@ -19,14 +19,16 @@
 
 /* VirtualBox includes */
 #include <VBoxDD.h>
+#include <KeyboardImpl.h>
+#include <MouseImpl.h>
 
 #include "vmm.h"
 
-#define REGISTER(device)                                       \
-	do {                                                       \
-		rc = pCallbacks->pfnRegister(pCallbacks, &g_##device); \
-		if (RT_FAILURE(rc))                                    \
-			return rc;                                         \
+#define REGISTER(driver) \
+	do { \
+		rc = pCallbacks->pfnRegister(pCallbacks, &driver); \
+		if (RT_FAILURE(rc)) \
+			return rc; \
 	} while (0)
 
 
@@ -34,23 +36,26 @@ extern "C" DECLEXPORT(int) VBoxDriversRegister(PCPDMDRVREGCB pCallbacks, uint32_
 {
 	int rc = 0;
 
-	REGISTER(DrvMouseQueue);
-	REGISTER(DrvKeyboardQueue);
-	REGISTER(DrvVD);
-	REGISTER(DrvHostDVD);
-	REGISTER(DrvHostInterface);
-	REGISTER(DrvAUDIO);
-	REGISTER(DrvHostNullAudio);
-	REGISTER(DrvACPI);
-	REGISTER(DrvAcpiCpu);
-	REGISTER(DrvVUSBRootHub);
-	REGISTER(DrvNamedPipe);
-	REGISTER(DrvTCP);
-	REGISTER(DrvUDP);
-	REGISTER(DrvRawFile);
-	REGISTER(DrvChar);
-	REGISTER(DrvHostSerial);
-	REGISTER(DrvIfTrace);
+	REGISTER(g_DrvMouseQueue);
+	REGISTER(g_DrvKeyboardQueue);
+	REGISTER(g_DrvVD);
+	REGISTER(g_DrvHostDVD);
+	REGISTER(g_DrvHostInterface);
+	REGISTER(g_DrvAUDIO);
+	REGISTER(g_DrvHostNullAudio);
+	REGISTER(g_DrvACPI);
+	REGISTER(g_DrvAcpiCpu);
+	REGISTER(g_DrvVUSBRootHub);
+	REGISTER(g_DrvNamedPipe);
+	REGISTER(g_DrvTCP);
+	REGISTER(g_DrvUDP);
+	REGISTER(g_DrvRawFile);
+	REGISTER(g_DrvChar);
+	REGISTER(g_DrvHostSerial);
+	REGISTER(g_DrvIfTrace);
+
+	REGISTER(Keyboard::DrvReg);
+	REGISTER(Mouse::DrvReg);
 
 	return VINF_SUCCESS;
 }
