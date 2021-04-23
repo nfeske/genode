b18da9eb43cbc12cca4472a3fa2f68496b5622ca
Norman Feske <norman.feske@genode-labs.com>
2021-04-23 14:09:09 +0200

    DEBUG wifi_drv instrumentation

--- repos/gems/src/app/sculpt_manager/runtime/wifi_drv.cc
+++ repos/gems/src/app/sculpt_manager/runtime/wifi_drv.cc
@@ -35,7 +35,7 @@ void Sculpt::gen_wifi_drv_start_content(Xml_generator &xml)
 		});
 
 		xml.node("libc", [&] () {
-			xml.attribute("stdout", "/dev/null");
+			xml.attribute("stdout", "/dev/log");
 			xml.attribute("stderr", "/dev/null");
 			xml.attribute("rtc",    "/dev/rtc");
 		});
