cc1923ad832eca15eb37488e5f47c5d9843f97e7
Norman Feske <norman.feske@genode-labs.com>
2023-03-14 15:27:27 +0100

    stdin2out: replace periodic operation by select
    This patch changes the stdin2out utility to forward input immediatedly
    instead of checking for data in intervals. It relies on 'select' for
    watching the readability of stdin.
    
    Issue #4785

--- repos/libports/src/app/stdin2out/main.cc
+++ repos/libports/src/app/stdin2out/main.cc
@@ -1,5 +1,5 @@
 /*
- * \brief  Utility for forwarding data to stdout by polling stdin
+ * \brief  Utility for forwarding data from stdin to stdout
  * \author Norman Feske
  * \date   2020-03-25
  *
@@ -13,19 +13,28 @@
  * under the terms of the GNU Affero General Public License version 3.
  */
 
+#include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <sys/select.h>
 
 extern "C" int main(int, char **)
 {
 	while (true) {
 
+		/* wait until stdin becomes ready to read */
+		fd_set fds { };
+		FD_ZERO(&fds);
+		FD_SET(STDIN_FILENO, &fds);
+		select(1, &fds, NULL, NULL, NULL);
+
 		char buffer[4096] { };
+		ssize_t const bytes = read(STDIN_FILENO, buffer, sizeof(buffer));
 
-		size_t bytes = read(STDIN_FILENO, buffer, sizeof(buffer));
+		if (bytes == -1)
+			fprintf(stderr, "read failed, errno=%d\n", errno);
 
-		if (bytes == 0)
-			sleep(2);
-		else
+		if (bytes > 0)
 			write(STDOUT_FILENO, buffer, bytes);
 	}
 }
