6f0370e6f86fd08d6c7cb79f12b1cbb4fcffe5f2
Norman Feske <norman.feske@genode-labs.com>
2023-07-24 18:13:48 +0200

    sculpt_manager: add sub dialog types
    Left_floating_text, Dialog::Right_floating_hbox, Vgap

--- repos/gems/src/app/sculpt_manager/view/dialog.h
+++ repos/gems/src/app/sculpt_manager/view/dialog.h
@@ -24,7 +24,16 @@ namespace Dialog {
 	struct Annotation;
 	struct Left_annotation;
 	struct Left_right_annotation;
+	struct Left_floating_text;
+	struct Left_floating_hbox;
+	struct Right_floating_hbox;
+	struct Vgap;
+	struct Centered_info_vbox;
+	struct Centered_dialog_vbox;
 	struct Titled_frame;
+	struct Pin_button;
+	struct Pin_row;
+	template <typename> struct Radio_select_button;
 }
 
 
@@ -77,6 +86,115 @@ struct Dialog::Left_right_annotation : Sub_scope
 };
 
 
+struct Dialog::Left_floating_text : Sub_scope
+{
+	template <typename SCOPE, typename TEXT>
+	static void view_sub_scope(SCOPE &s, TEXT const &text)
+	{
+		s.node("float", [&] {
+			s.attribute("west", "yes");
+			s.named_sub_node("label", "label", [&] {
+				s.attribute("text", String<30>("  ", text));
+				s.attribute("min_ex", "15"); }); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &, FN const &) { }
+};
+
+
+struct Dialog::Left_floating_hbox : Sub_scope
+{
+	template <typename SCOPE, typename FN>
+	static void view_sub_scope(SCOPE &s, FN const &fn)
+	{
+		s.node("float", [&] {
+			s.attribute("west", "yes");
+			s.named_sub_node("hbox", s.id.value, [&] {
+				fn(s); }); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &at, FN const &fn)
+	{
+		with_narrowed_xml(at, "float", [&] (AT const &at) {
+			with_narrowed_xml(at, "hbox", fn); });
+	}
+};
+
+
+struct Dialog::Right_floating_hbox : Sub_scope
+{
+	template <typename SCOPE, typename FN>
+	static void view_sub_scope(SCOPE &s, FN const &fn)
+	{
+		s.node("float", [&] {
+			s.attribute("east", "yes");
+			s.named_sub_node("hbox", s.id.value, [&] {
+				fn(s); }); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &at, FN const &fn)
+	{
+		with_narrowed_xml(at, "float", [&] (AT const &at) {
+			with_narrowed_xml(at, "hbox", fn); });
+	}
+};
+
+
+struct Dialog::Vgap : Sub_scope
+{
+	template <typename SCOPE>
+	static void view_sub_scope(SCOPE &s)
+	{
+		s.node("label", [&] { s.attribute("text", " "); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &, FN const &) { }
+};
+
+
+struct Dialog::Centered_info_vbox : Sub_scope
+{
+	template <typename SCOPE, typename FN>
+	static void view_sub_scope(SCOPE &s, FN const &fn)
+	{
+		s.node("float", [&] {
+			s.sub_node("frame", [&] {
+				s.attribute("style", "unimportant");
+				s.named_sub_node("vbox", s.id.value, [&] {
+					fn(s); }); }); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &, FN const &) { }
+};
+
+
+struct Dialog::Centered_dialog_vbox : Sub_scope
+{
+	template <typename SCOPE, typename FN>
+	static void view_sub_scope(SCOPE &s, FN const &fn)
+	{
+		s.node("float", [&] {
+			s.sub_node("frame", [&] {
+				s.attribute("style", "important");
+				s.named_sub_node("vbox", s.id.value, [&] {
+					fn(s); }); }); });
+	}
+
+	template <typename AT, typename FN>
+	static void with_narrowed_at(AT const &at, FN const &fn)
+	{
+		with_narrowed_xml(at, "float", [&] (AT const &at) {
+			with_narrowed_xml(at, "frame", [&] (AT const &at) {
+				with_narrowed_xml(at, "vbox", fn); }); });
+	}
+};
+
+
 struct Dialog::Titled_frame : Widget<Frame>
 {
 	struct Attr { unsigned min_ex; };
@@ -98,6 +216,85 @@ struct Dialog::Titled_frame : Widget<Frame>
 	static void view(Scope<Frame> &s, FN const &fn)
 	{
 		view(s, Attr { }, fn);
+	}
+};
+
+
+template <typename ENUM>
+struct Dialog::Radio_select_button : Widget<Left_floating_hbox>
+{
+	ENUM const value;
+
+	Radio_select_button(ENUM value) : value(value) { }
+
+	template <typename TEXT>
+	void view(Scope<Left_floating_hbox> &s, ENUM const &selected_value, TEXT const &text) const
+	{
+		bool const selected = (selected_value == value),
+		           hovered  = (s.hovered() && !s.dragged() && !selected);
+
+		s.sub_scope<Float>([&] (Scope<Left_floating_hbox, Float> &s) {
+			s.sub_scope<Button>([&] (Scope<Left_floating_hbox, Float, Button> &s) {
+				s.attribute("style", "radio");
+
+				if (selected) s.attribute("selected", "yes");
+				if (hovered)  s.attribute("hovered",  "yes");
+
+				s.sub_scope<Hbox>();
+			});
+		});
+
+		/* inflate vertical space to button size */
+		s.sub_scope<Button>([&] (Scope<Left_floating_hbox, Button> &s) {
+			s.attribute("style", "invisible");
+			s.sub_scope<Label>(text); });
+	}
+
+	template <typename FN>
+	void click(Clicked_at const &, FN const &fn) { fn(value); }
+};
+
+
+struct Dialog::Pin_button : Action_button
+{
+	void view(Scope<Button> &s) const
+	{
+		auto const &text = s.id.value;
+
+		Action_button::view(s, [&] (Scope<Button> &s) {
+			s.sub_scope<Vbox>([&] (Scope<Button, Vbox> &s) {
+				s.sub_scope<Min_ex>(10);
+				s.sub_scope<Vgap>();
+				s.sub_scope<Dialog::Label>(text, [&] (auto &s) {
+					s.attribute("font", "title/regular"); });
+				s.sub_scope<Vgap>();
+			});
+		});
+	}
+};
+
+
+struct Dialog::Pin_row : Widget<Hbox>
+{
+	Hosted<Hbox, Pin_button> _buttons[3];
+
+	template <typename S1, typename S2, typename S3>
+	Pin_row(S1 const &left, S2 const &middle, S3 const &right)
+	:
+		_buttons { Id { left }, Id { middle }, Id { right } }
+	{ }
+
+	void view(Scope<Hbox> &s) const
+	{
+		for (auto const &button : _buttons)
+			s.widget(button);
+	}
+
+	template <typename FN>
+	void click(Clicked_at const &at, FN const &fn)
+	{
+		for (auto &button : _buttons)
+			button.propagate(at, [&] { fn(button.id.value); });
 	}
 };
 
