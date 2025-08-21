c0b319413b2f0206813cdbd5e90ea643e4d11a7a
Norman Feske <norman.feske@genode-labs.com>
2025-08-21 10:31:07 +0200

    fixup "Parser and generator for Human-Readable Data (HRD)" (pkg/test-hrd)

--- repos/base/recipes/src/test-hrd/content.mk
+++ repos/base/recipes/src/test-hrd/content.mk
@@ -1,2 +1,2 @@
-SRC_DIR = src/test/hrd_node
+SRC_DIR = src/test/hrd
 include $(GENODE_DIR)/repos/base/recipes/src/content.inc
--- repos/base/src/test/hrd/target.mk
+++ repos/base/src/test/hrd/target.mk
@@ -1,3 +1,3 @@
-TARGET = test-hrd_node
+TARGET = test-hrd
 SRC_CC = main.cc
 LIBS  += base
