c846305fecd94d19db6f1733c518205f1cf4d146
Norman Feske <norman.feske@genode-labs.com>
2025-11-17 12:10:38 +0100

    Revert "fixup depot_autopilot? (strip escape sequences)"
    This reverts commit 35949012c4d68672293de9c98eb3c6496999cd34.

--- repos/gems/src/app/depot_autopilot/string_chain.h
+++ repos/gems/src/app/depot_autopilot/string_chain.h
@@ -33,17 +33,9 @@ struct Depot_autopilot::String_chain
 
 		static Byte_range_ptr _copied(Allocator &alloc, Span const &span)
 		{
-			size_t const n = num_unescaped_bytes(Cstring(span.start, span.num_bytes));
-
-			Byte_range_ptr bytes { (char *)alloc.alloc(n), n };
-			bool const ok = bytes.as_output([&] (Output &out) {
-				Unescaped_output unescaped { out };
-				Genode::print(unescaped, Cstring(span.start, span.num_bytes));
-			}).ok();
-
-			if (!ok) warning("failed to strip escape sequence from log output");
-
-			return { bytes.start, bytes.num_bytes };
+			char *dst = (char *)alloc.alloc(span.num_bytes);
+			memcpy(dst, span.start, span.num_bytes);
+			return { dst, span.num_bytes };
 		}
 
 		Element(Allocator &alloc, Span const &span)
